4255fac131bfebe2f235dcc094b942925985cc56
Meghdad <mmf9484943@gmail.com>
2026-05-06 07:48:19 +0330

    increment version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,8 +33,8 @@ android {
         applicationId = "net.rodakot.posthub"
         minSdk = 24
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0.0"
+        versionCode = 101000
+        versionName = "1.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
